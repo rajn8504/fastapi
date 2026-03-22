47de35a1c0d2411b4a4fba9654174c8c56bbb535
rajn8504 <rajn8504@gmail.com>
2026-03-22 13:57:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,6 @@ async def root():
     current_time = datetime.now(ist).strftime('%Y-%m-%d %H:%M:%S')
     return {"status": "Algo_Live", "ist_time": current_time}
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
